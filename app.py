3a57e6a693f87969a0c62112c2a185f7ba7c324a
labhanshi003 <labhanshisahu811@gmail.com>
2026-02-05 22:20:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from db import get_db_connection
+from db import get_connection
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/book", methods=["POST"])
-def book_appointment():
+def book():
     data = request.json
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    conn = get_connection()
+    cur = conn.cursor()
 
-    sql = """
-    INSERT INTO bookings (name, phone, service, date, time, message)
-    VALUES (%s, %s, %s, %s, %s, %s)
-    """
-    values = (
+    cur.execute("""
+        INSERT INTO bookings (name, phone, service, date, time)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (
         data["name"],
         data["phone"],
         data["service"],
         data["date"],
-        data["time"],
-        data["message"]
-    )
+        data["time"]
+    ))
 
-    cursor.execute(sql, values)
     conn.commit()
-
-    cursor.close()
+    cur.close()
     conn.close()
 
-    return jsonify({"message": "Booking successful"}), 201
+    return jsonify({"message": "Appointment booked successfully!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
