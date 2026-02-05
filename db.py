4a8993b2b658ea5f073ac790c85a4fb53490c5cb
labhanshi003 <labhanshisahu811@gmail.com>
2026-02-05 22:20:10 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,7 @@
-import mysql.connector
+import psycopg2
+import os
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="krishvika_makeover"
-    )
+DATABASE_URL = os.environ.get("postgresql://krishvika_db_user:d8vPEO77hkNjM2u9UsrUHYhSbIyOBpkf@dpg-d62ciq8nputs739l31dg-a/krishvika_db")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
